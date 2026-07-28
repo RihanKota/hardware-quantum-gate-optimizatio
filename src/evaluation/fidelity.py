ad6a176e3c424353264f361a4523516067d61de0
Rihan Kota <rihankota2004@gmail.com>
2026-07-28 12:18:35 +0530

    update

--- src/evaluation/fidelity.py
+++ src/evaluation/fidelity.py
@@ -1,13 +1,6 @@
 import numpy as np
 
 
-<<<<<<< HEAD
-def calculate_fidelity(
-        state
-):
-
-    target=np.array(
-=======
 def state_fidelity(final_state, target_state):
 
     final_state = final_state / np.linalg.norm(final_state)
@@ -29,26 +22,10 @@ def x_gate_target():
     # |0> -> |1>
 
     return np.array(
->>>>>>> 611e7cc82fd5e6dda4b77c9ea91044fb78fe8b13
         [
             0,
             1,
             0
         ],
         dtype=complex
-<<<<<<< HEAD
     )
-
-
-    fidelity=abs(
-        np.vdot(
-            target,
-            state
-        )
-    )**2
-
-
-    return fidelity
-=======
-    )
->>>>>>> 611e7cc82fd5e6dda4b77c9ea91044fb78fe8b13
--- src/pulses/drag.py
+++ src/pulses/drag.py
@@ -1,26 +1,5 @@
 import numpy as np
 
-<<<<<<< HEAD
-
-def drag_pulse(
-        sigma=20,
-        beta=0.5,
-        duration=100
-):
-
-    t=np.arange(duration)
-
-    center=duration/2
-
-
-    gaussian=np.exp(
-        -(t-center)**2 /
-        (2*sigma**2)
-    )
-
-
-    derivative=-(t-center)/(sigma**2)*gaussian
-=======
 def drag_pulse(
         sigma=20,
         beta=0.5,
@@ -42,19 +21,11 @@ def drag_pulse(
 
 
     derivative = np.gradient(gaussian)
->>>>>>> 611e7cc82fd5e6dda4b77c9ea91044fb78fe8b13
 
 
     pulse = (
         gaussian
         +
-<<<<<<< HEAD
-        1j*beta*derivative
-    )
-
-
-    return pulse
-=======
         1j * beta * derivative
     )
 
@@ -62,4 +33,3 @@ def drag_pulse(
 
 
     return t,pulse
->>>>>>> 611e7cc82fd5e6dda4b77c9ea91044fb78fe8b13
